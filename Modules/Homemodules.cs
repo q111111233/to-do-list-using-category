6c4ab276e5ac7c97c497be187b12122c460907f4
Bhawani Parajuli and bo zhao <bhawani+bo@>
2016-06-30 10:00:12 -0700

    updated to do list that has categories and tasks

---
+++ Modules/Homemodules.cs
@@ -0,0 +1,55 @@
+using Nancy;
+using System.Collections.Generic;
+using ToDoList.Objects;
+
+namespace ToDoList
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"]=_=>{
+        return View["index.cshtml"];
+      };
+      Get["/categories"]=_=>{
+        var allCategories = Category.GetAll();
+        return View["categories.cshtml", allCategories];
+      };
+      Get["/categories/new"]=_=>{
+        return View["category_form.cshtml"];
+      };
+      Post["/categories"]=_=>{
+        var newCategory = new Category(Request.Form["category-name"]);
+        var allCategories = Category.GetAll();
+        return View["categories.cshtml", allCategories];
+      };
+      Get["/categories/{id}"]= parameters =>{
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var selectedCategory = Category.Find(parameters.id);
+        var categoryTasks = selectedCategory.GetTasks();
+        model.Add("category", selectedCategory);
+        model.Add("tasks", categoryTasks);
+        return View["category.cshtml", model];
+      };
+      Get["/categories/{id}/tasks/new"]= parameters =>{
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Category selectedCategory = Category.Find(parameters.id);
+        List<Task> allTasks = selectedCategory.GetTasks();
+        model.Add("category", selectedCategory);
+        model.Add("tasks", allTasks);
+        return View["category_task_form.cshtml", model];
+      };
+      Post["/tasks"]=_=>{
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Category selectedCategory = Category.Find(Request.Form["category-id"]);
+        List<Task> categoryTasks = selectedCategory.GetTasks();
+        string taskDescription = Request.Form["taskDescription"];
+        Task newTask = new Task(taskDescription);
+        categoryTasks.Add(newTask);
+        model.Add("tasks", categoryTasks);
+        model.Add("category", selectedCategory);
+        return View["category.cshtml", model];
+      };
+    }
+  }
+}
---
+++ Objects/Task.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+
+namespace ToDoList
+{
+  public class Task
+  {
+    private string _description;
+    private int _id;
+    private static List<Task> _instances = new List<Task> {};
+
+    public Task(string description)
+    {
+      _description = description;
+      _instances.Add(this);
+      _id = _instances.Count;
+    }
+
+    public string GetDescription()
+    {
+      return _description;
+    }
+
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static List<Task> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Task Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+  }
+}
